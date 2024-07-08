08fe9db961bd688c71c35d12cfa2caf6b27437b8
Shahrokh Hamidi <156338354+Shahrokh-Hamidi@users.noreply.github.com>
2024-07-08 17:07:30 -0400

    Add files via upload

---
+++ miscellaneous/time_series_analysis.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cvxpy as cp
+import matplotlib 
+
+
+#y = np.loadtxt('D:/code/optimization/CVX-master/examples/time_series_analysis/snp500.txt')
+n = 2000
+y = np.zeros(n)
+A0 = 1
+for i in range(1,n):
+    y[i] = A0*y[i-1] + np.random.normal(0,0.05)
+
+n = len(y)
+
+
+A = np.hstack((np.array([1, -2, 1]), np.zeros(n-3)))
+
+D = [] 
+for i in range(len(A)):
+    D.append(np.roll(A,i))
+
+D = np.array(D)
+
+
+
+lambda_ = 10
+x = cp.Variable(n)
+
+cost = cp.norm(y - x, 2) + lambda_* cp.sum(cp.abs(D@x))      #cp.tv(x)
+constr = []
+prob = cp.Problem(cp.Minimize(cost), constr) 
+prob.solve(verbose = True)
+
+
+
+x = x.value
+
+plt.rcParams['savefig.dpi'] = 300
+
+plt.plot(y, 'k-', lw = 0.3)
+plt.plot(x, 'r')
+matplotlib.rc('font', size=14)
+plt.title('Time Series Analysis')
+
+plt.xlabel('t', fontsize = 16)
+plt.ylabel('y[t]', fontsize = 16)
+matplotlib.style.use('ggplot')
+plt.show()
+
